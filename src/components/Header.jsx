434632edeebc9b00ab7409405832007d3e04b495
Rafflafressia <149746567+Rafflafressia@users.noreply.github.com>
2024-04-20 22:22:14 -0400

    Home successfully renders.

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <header className="header">
+            <h1>My Blog</h1>
+            <p>Welcome to my blog</p>
+        </header>
+    );
+}
+
+export default Header;
