0e0b915bc12879f97824194d45dab6cb02ca553a
Rafflafressia <149746567+Rafflafressia@users.noreply.github.com>
2024-04-27 02:13:44 -0400

    Updated About Page

--- src/components/GraphicDesignCards.jsx
+++ src/components/GraphicDesignCards.jsx
@@ -1,30 +1,40 @@
-const graphicDesignProjects = [{
-    title: 'Design Project 1',
-    description: 'This is project 1',
-    imageUrl: 'https://via.placeholder.com/150',
-},
-{
-    title: 'Design Project 2',
-    description: 'This is project 2',
-    imageUrl: 'https://via.placeholder.com/150',
-}];
+const graphicDesignProjects = [
+  {
+    title: "Design Project 1",
+    description: "This is project 1",
+    imageUrl: "https://via.placeholder.com/150",
+  },
+  {
+    title: "Design Project 2",
+    description: "This is project 2",
+    imageUrl: "https://via.placeholder.com/150",
+  },
+];
 
 const GraphicDesignCards = () => {
-    return (
-        <div className="graphic-design-cards">
-            {graphicDesignProjects.map((project, index) => (
-                <div key={index} className="graphic-design-card flex my-4 mx-4 space-x-2 bg-cyan-950">
-                    <div className="projImg">
-                        <img src={project && project.imageUrl} alt={project && project.title} />
-                    </div>
-                    <div className="projDesc">
-                        <h3>{project && project.title}</h3>
-                        <p>{project && project.description}</p>
-                    </div>
-                </div>
-            ))}
+  return (
+    <div className="graphic-design-cards">
+      {graphicDesignProjects.map((project, index) => (
+        <div
+          key={index}
+          className="graphic-design-card flex flex-col md:col items-center text-center w-[300px] mx-auto md:mx-[50px] my-12 bg-cyan-950 border-cyan-300 border-2"
+        >
+          <div className="projImg w-[150px] h-[150px] my-4">
+            <img
+              src={project && project.imageUrl}
+              alt={project && project.title}
+            />
+          </div>
+          <div className="projDesc py-2">
+            <h3 className="text-2xl text-white font-bold my-2 md:mx-4">
+              {project && project.title}
+            </h3>
+            <p className="text-white">{project && project.description}</p>
+          </div>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default GraphicDesignCards;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,17 @@
 const About = () => {
   return (
-    <div className="h-[500px] md:h-[450px] lg:h-[593px] xl:h-[883px] ">
-      <h1>About</h1>
-      <p>This is the about page.</p>
+    <div className="h-[475px] md:h-[450px] lg:h-[589px] xl:h-[878px] ">
+      <div className="about-box flex flex-col items-center my-6">
+        <h1 className="text-4xl font-bold text-white">
+          This is <span className="text-cyan-300">me</span>
+        </h1>
+        <p className="my-12 text-xl text-white mx-10 text-left">
+          I am a budding UI/UX designer. My passion is in illustrations, graphic
+          designs and application designs.
+        </p>
+      </div>
     </div>
   );
-}
+};
 
 export default About;
