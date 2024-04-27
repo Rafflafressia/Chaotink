b6f3255843326506db5b5c2b2d5dac14e1849104
Rafflafressia <149746567+Rafflafressia@users.noreply.github.com>
2024-04-27 02:13:44 -0400

    Updated About Page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,15 +2,19 @@ const Footer = () => {
   return (
     <footer className="flex mx-auto border-t-2 border-cyan-300 items-center justify-between">
       <p className="m-[10px] font-semibold text-white">Visit My Socials!</p>
-      <ul className="socials flex space-x-4 mx-4 text-white">
-        <a href="#"><li className="linkedIn">LinkedIn</li></a>
-        <a href="#"><li className="github">GitHub</li></a>
-        <a href="#"><li className="instagram">Instagram</li></a>
+      <ul className="socials flex space-x-4 mx-2 text-white">
+        <a href="#">
+          <li className="linkedIn">LinkedIn</li>
+        </a>
+        <a href="#">
+          <li className="github">GitHub</li>
+        </a>
+        <a href="#">
+          <li className="instagram">Instagram</li>
+        </a>
       </ul>
     </footer>
   );
-}
-
-
+};
 
 export default Footer;
--- src/components/PortfolioSubNavigation.jsx
+++ src/components/PortfolioSubNavigation.jsx
@@ -1,30 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const PortfolioSubNavigation = ({ categories, onSelectCategory }) => {
-    const [selectedCategory, setSelectedCategory] = useState(categories[0]);
+  const [selectedCategory, setSelectedCategory] = useState(categories[0]);
 
-    const handleCategorySelect = (category) => {
-        setSelectedCategory(category);
-        onSelectCategory(category);
-    };
+  const handleCategorySelect = (category) => {
+    setSelectedCategory(category);
+    onSelectCategory(category);
+  };
 
-    return (
-        <aside className="portfolio-sub-navigation md:border-r-2 md:border-cyan-300">
-            <ul className="flex justify-center space-x-2 my-[10px] mx-auto md:mx-[25px] md:block md:my-[50px] w-[200px] md:text-right md:px-2 items-center">
-                {categories.map((category, index) => (
-                    <li
-                        key={index}
-                        className={`text-sm p-2 rounded-md md:mx-1 md:border-b-2 md:border-cyan-300 md:p-2 ${category === selectedCategory ? 'active text-cyan-300' : 'text-white'} hover:text-cyan-300 hover:bg-cyan-950`}
-                        onClick={() => handleCategorySelect(category)}
-                    ><button>
-                        {category}
-                    </button>
-                    </li>
-                ))}
-            </ul>
-        </aside>
-    );
+  return (
+    <aside className="portfolio-sub-navigation md:border-r-2 md:border-cyan-300">
+      <ul className="flex justify-center space-x-2 mx-auto md:mx-[25px] md:block md:my-[50px] w-[200px] md:text-right md:px-2 items-center">
+        {categories.map((category, index) => (
+          <li
+            key={index}
+            className={`text-sm p-2 rounded-md md:mx-1 md:border-b-2 md:border-cyan-300 md:p-2 ${
+              category === selectedCategory
+                ? "active text-cyan-300"
+                : "text-white"
+            } hover:text-cyan-300`}
+            onClick={() => handleCategorySelect(category)}
+          >
+            <button>{category}</button>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
 };
-
 
 export default PortfolioSubNavigation;
