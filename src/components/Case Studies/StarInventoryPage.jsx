63858d692612863943debf1b03204fbd3723396b
Rafflafressia <149746567+Rafflafressia@users.noreply.github.com>
2024-07-25 22:54:20 -0400

    Added Star Inventory page.

--- src/components/Case Studies/StarInventoryPage.jsx
+++ src/components/Case Studies/StarInventoryPage.jsx
@@ -9,6 +9,7 @@ import Footer from "../Footer";
 import Navbar from "../Navbar";
 
 import StarBG from "../../assets/sil-bg.png";
+import StarLogo from "../../assets/star-inventory-logo.svg";
 
 const StarInventoryPage = () => {
   useEffect(() => {
@@ -18,7 +19,7 @@ const StarInventoryPage = () => {
   return (
     <div>
       <div
-        className="w-full py-12"
+        className={`w-full p-4 flex flex-col space-y-4 items-center justify-center bg-opacity-30`}
         id="home"
         style={{
           backgroundImage: `url(${StarBG})`,
@@ -26,7 +27,15 @@ const StarInventoryPage = () => {
           backgroundPosition: "center",
           backgroundRepeat: "no-repeat",
         }}
-      ><h1>Star Inventory Systems is an inventory management system for small businesses. This application was made to keep track of inventory, dynamically update pricing and help with quoting and onboarding of new staff.</h1></div>
+      >
+        <img src={StarLogo} alt="Star Inventory Logo" className="w-10/12" />
+        <h1 className="bg-opacity-60 p-2 font-medium shadow-sm bg-slate-50 rounded-[10px]">
+          Star Inventory Systems is an inventory management system for small
+          businesses. This application was made to keep track of inventory,
+          dynamically update pricing and help with quoting and onboarding of new
+          staff.
+        </h1>
+      </div>
 
       <Footer />
     </div>
