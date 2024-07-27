7d9be583caaca5bc428c761d84ad9bc44aec6909
Rafflafressia <149746567+Rafflafressia@users.noreply.github.com>
2024-07-26 22:11:12 -0400

    Updated Star page.

--- src/components/Case Studies/StarInventoryPage.jsx
+++ src/components/Case Studies/StarInventoryPage.jsx
@@ -16,8 +16,11 @@ const StarInventoryPage = () => {
     window.scrollTo(0, 0);
   }, []);
 
+  const headingsOne = "project text-2xl font-bold";
+  const headingsTwo = "font-medium";
+  
   return (
-    <div>
+    <div className="space-y-4">
       <div
         className={`w-full p-4 flex flex-col space-y-4 items-center justify-center bg-opacity-30`}
         id="home"
@@ -29,12 +32,48 @@ const StarInventoryPage = () => {
         }}
       >
         <img src={StarLogo} alt="Star Inventory Logo" className="w-10/12" />
-        <h1 className="bg-opacity-60 p-2 font-medium shadow-sm bg-slate-50 rounded-[10px]">
+        <p className="bg-opacity-60 p-2 font-medium shadow-sm bg-slate-50 rounded-[10px]">
           Star Inventory Systems is an inventory management system for small
-          businesses. This application was made to keep track of inventory,
-          dynamically update pricing and help with quoting and onboarding of new
-          staff.
-        </h1>
+          businesses. This application uses RESTful API operations. Users are
+          able to add, update, delete, and view inventory items. The system also
+          allows for the creation of new users and the ability to update user
+          information.
+        </p>
+      </div>
+      <div className="border-t-2 border-gray-300 py-4 m-4 space-y-4">
+        <h1 className={headingsOne}>Target Audience</h1>
+        <p>
+          The target audience for this application was for small print studios
+          that see high inventory, high turnover and a low employee count.
+        </p>
+
+        <h1 className={headingsOne}>Project Goal</h1>
+        <p>
+          Having worked in the print industry for over 10 years there are three
+          things consistent across each studio. Poor inventory management, high
+          employee turnover and steep learning curve. This application was meant
+          to solve that issue by making it user friendly
+        </p>
+
+        <h2 className={headingsTwo}>The Solutions</h2>
+        <ol className="list-decimal list-inside">
+          <li>
+            An easy way for employees to update and track inventory; new and
+            old,
+          </li>
+          <li>
+            Preprogrammed calculations for pricing on inventory items to let
+            employees look up pricing easily,
+          </li>
+          <li>
+            User friendly design to allow new employees to quickly and easily
+            access industry information.
+          </li>
+        </ol>
+      </div>
+
+      <div className="border-t-2 border-gray-300 py-4 m-4 space-y-4">
+        <h2 className={headingsOne}>What We Achieved</h2>
       </div>
 
       <Footer />
